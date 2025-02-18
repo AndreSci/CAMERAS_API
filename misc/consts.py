e2d897b45f02de9d72451806573cb6ff3f99efc8
AndreSci <scitecever@gmail.com>
2025-02-17 23:17:18 -0500

    Пересобранно под python3.10 и Docker

--- misc/consts.py
+++
@@ -1,3 +0,0 @@
-ERROR_ACCESS_IP = 'access_block_ip'
-ERROR_READ_REQUEST = 'error_read_request'
-ERROR_ON_SERVER = 'server_error'
--- tests/001.py
+++
@@ -1,6 +0,0 @@
-import datetime
-cam_name = "cam1"
-
-date_time = str(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
-
-print(f"{cam_name}-{date_time}")
